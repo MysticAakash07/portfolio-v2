ab416f427906dad0b38b72800185cda05148eb8c
MysticAakash07 <rahulsagar460@gmail.com>
2025-03-10 21:01:53 +0530

    Created the Experience Section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Technologies from "./components/Technologies";
+import Experience from "./components/Experience";
 function App() {
 	return (
 		<div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
@@ -16,6 +17,7 @@ function App() {
 				<Hero />
 				<About />
 				<Technologies />
+				<Experience />
 			</div>
 		</div>
 	);
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
 						<span className="bg-gradient-to-r from-pink-300 via-slate-500  to-purple-500  bg-clip-text text-4xl tracking-tight text-transparent">
 							Full Stack Developer
 						</span>
-						<p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+						<p className="my-2 max-w-xl py-6 font-light  tracking-tighter">
 							{HERO_CONTENT}
 						</p>
 					</div>
